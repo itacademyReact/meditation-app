980921fe8b4666e6a43d31c60e9015aaf862adfd
BAsingulars <singulars@gmail.com>
2021-05-13 12:04:01 +0200

    create js structure

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* VARIABLES */
+const video...
+const play...
+const progressCircle...
+const timeDisplay...
+const timeSelect (array de botones de seleccion de duracion)...
+let duration = 600;
+
+/* HANDLE LISTENERS */
+
+/* Play/pause */
+play.addEventListener("click", function(){
+    checkPlaying(video)
+})
+/* Time Select */
+timeSelect.addEventListener()
+
+/* LÓGICA */
+function checkPlaying() {
+
+}
+
+/* L1. Hendle events */
+/* Tengo que arrancar o pausar todo cuando el usuario lo requiera */
+/* Arrancar video, iniciar cuenta atrás, y progress circle */
+
+
+/* L2.Mostrar datos al usuario */
+
+
